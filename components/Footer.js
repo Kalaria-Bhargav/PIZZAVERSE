186050470330269b74d79af22636b7813f628bec
xxMagnetoxx <70790306+xxMagnetoxx@users.noreply.github.com>
2022-08-28 00:34:18 +0530

    Removed Extra space

--- components/Footer.js
+++ components/Footer.js
@@ -8,14 +8,15 @@ const Footer = () => {
         <div className={style.container}>
             <span>ENJOY PIZZAS</span>
             <div className={style.social}>
-                <UilFacebook size={45}/>
-                <UilLinkedin size={45}/>
-                <UilInstagram size={45}/>
+               <a   href='https://www.facebook.com/bhargav.kalaria.7/' target="_blank" rel="noopener noreferrer"> <UilFacebook size={45}/></a>
+                <a href='https://www.linkedin.com/in/bhargav-kalaria-90587a19b' target="_blank" rel="noopener noreferrer"><UilLinkedin size={45}/></a>
+                <a href='https://www.instagram.com/ig_devil07/' target="_blank" rel="noopener noreferrer" ><UilInstagram size={45}/></a>
             </div>
             <div className={style.logo}>
             <Image src={Logo} style={{borderRadius:'5px'}} alt="" width={40} height={40}/>
             <span>PIZZAVERSE</span>
         </div> 
+            <div></div>
         </div>
     )
 }
